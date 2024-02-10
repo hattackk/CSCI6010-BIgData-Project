aecc5130a4529ae20a9c77cadc920ae4a66d2668
hattackk <36685328+hattackk@users.noreply.github.com>
2024-02-10 15:45:47 -0500

    Added reviews api call (#2)

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ app_list = steam_api_client.get_app_list()
 
 
 for i,app in enumerate(app_list):
-    if app['appid'] == 1091500:
-        print(f'Found Cyberpunk 2077 at {i} {app}')
+    if app['appid'] == 329070:
+        print(f'Found application {app.get("name")} at {i} {app}')
 
 import json
-print(json.dumps(steam_api_client.get_news_for_app(1091500),indent=4))
+response = steam_api_client.get_reviews_for_app(app_id=329070, num_per_page=1)
+print(response.get('reviews'))
 
     
 
--- steam_api_client.py
+++ steam_api_client.py
@@ -13,22 +13,30 @@ class SteamAPIClient:
             api_key (str, optional): The API key for accessing the Steam Web API.
         """
         self.base_url = "https://api.steampowered.com"
+        self.reviews_base_url = "https://store.steampowered.com/"
         self.api_key = api_key
 
-    def _make_request(self, interface, method, version, params=None):
+    def _make_request(self, base_url, interface=None, method=None, version=None, params=None):
         """
         Makes a request to the Steam Web API.
 
         Parameters:
-            interface (str): The interface of the API.
-            method (str): The method of the API.
-            version (int): The version of the API.
+            base_url (str): The base URL of the API.
+            interface (str, optional): The interface of the API.
+            method (str, optional): The method of the API.
+            version (int, optional): The version of the API.
             params (dict, optional): Additional parameters to include in the request.
 
         Returns:
             dict or None: The JSON response from the API, or None if the request fails.
         """
-        url = f"{self.base_url}/{interface}/{method}/v{version}/"
+        url = f"{base_url}/"
+        if interface:
+            url += f"{interface}/"
+        if method:
+            url += f"{method}/"
+        if version:
+            url += f"v{version}/"
         if self.api_key:
             params = params or {}
             params['key'] = self.api_key
@@ -54,7 +62,7 @@ class SteamAPIClient:
             'appid': app_id,
             'count': count
         }
-        return self._make_request("ISteamNews", "GetNewsForApp", 2, params=params)
+        return self._make_request(self.base_url, "ISteamNews", "GetNewsForApp", 2, params=params)
     
     def get_app_list(self):
         """
@@ -66,11 +74,40 @@ class SteamAPIClient:
                     - appid (int): The App ID of the application.
                     - name (str): The name of the application.
         """
-        response = self._make_request("ISteamApps", "GetAppList", 2)
+        response = self._make_request(self.base_url, "ISteamApps", "GetAppList", 2)
         if response:
             return response.get('applist', {}).get('apps', [])
         else:
             return []
 
+    def get_reviews_for_app(self, app_id, filter="all", language="all", day_range=None, cursor="*", 
+                            review_type="all", purchase_type="steam", num_per_page=20, filter_offtopic_activity=None):
+        """
+        Retrieves reviews for a specific app from the Steam Web API.
 
+        Parameters:
+            app_id (int): The AppID of the game or application.
+            filter (str): The filter for reviews (default is "all").
+            language (str): The language of reviews (default is "all").
+            day_range (str): The range from now to n days ago to look for helpful reviews.
+            cursor (str): The cursor value to retrieve the next batch of reviews (default is "*").
+            review_type (str): The type of reviews (default is "all").
+            purchase_type (str): The purchase type of reviews (default is "steam").
+            num_per_page (int): The number of reviews per page (default is 20).
+            filter_offtopic_activity (bool): Whether to include off-topic reviews (default is None).
 
+        Returns:
+            dict or None: The JSON response containing reviews, or None if the request fails.
+        """
+        params = {
+            "json": "1",
+            'filter': filter,
+            'language': language,
+            'day_range': day_range,
+            'cursor': cursor,
+            'review_type': review_type,
+            'purchase_type': purchase_type,
+            'num_per_page': num_per_page,
+            'filter_offtopic_activity': filter_offtopic_activity
+        }
+        return self._make_request(self.reviews_base_url, interface="appreviews", method=str(app_id), version=None, params=params)
