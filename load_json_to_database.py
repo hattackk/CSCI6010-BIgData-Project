49b4a3a8f69751c0b073e4353ab9f14b5cfe5b92
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2024-02-15 11:44:16 -0500

    query summary is incorrect. pushing code to share

--- load_json_to_database.py
+++ load_json_to_database.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 from dotenv import load_dotenv
 import click
@@ -45,16 +45,15 @@ def add_or_update(record_list: List[Dict], table: Table, conn):
     conn.commit()
 
 
-def process_game_file(games_file, conn):
-    # load the games_file
-    with open(games_file, 'r') as f:
-        games_file = json.loads(f.read())
-
-    # format the game data to match the game table
+def parse_game_json(game_json) -> Tuple[List, List, List]:
+    """
+    :param game_json: dictionary of game_json with appid as the key
+    :return: games, games_review_summary, game_rating each as a list
+    """
     games = []
     games_review_summary = []
     game_rating = []
-    for key, value in games_file.items():
+    for key, value in game_json.items():
 
         # Here is what is needed for the games table
         games.append(
@@ -97,9 +96,54 @@ def process_game_file(games_file, conn):
                 median_2weeks=value.get('median_2weeks')
             )
         )
+
+    return games, games_review_summary, game_rating
+
+
+def process_game_file(games_file, conn):
+    # load the games_file
+    with open(games_file, 'r') as f:
+        games_file = json.loads(f.read())
+
+    games, games_review_summary, game_rating = parse_game_json(games_file)
     add_or_update(games, games_table, conn)
     add_or_update(game_rating, game_rating_table, conn)
     add_or_update(games_review_summary, game_review_summary_table, conn)
+
+
+def parse_single_game_review(json_obj) -> Tuple[Dict, Dict]:
+    user = json_obj.get('author')
+
+    single_review = dict(
+            recommendationid=json_obj.get('recommendationid'),
+            author=user.get('steamid'),
+            language=json_obj.get('language'),
+            review=json_obj.get('review'),
+            timestamp_created=json_obj.get('timestamp_created'),
+            timestamp_updated=json_obj.get('timestamp_updated'),
+            voted_up=json_obj.get('voted_up'),
+            votes_up=json_obj.get('votes_up'),
+            votes_funny=json_obj.get('votes_funny'),
+            weighted_vote_score=json_obj.get('weighted_vote_score'),
+            comment_count=json_obj.get('comment_count'),
+            steam_purchase=json_obj.get('steam_purchase'),
+            received_for_free=json_obj.get('received_for_free'),
+            written_during_early_access=json_obj.get('written_during_early_access'),
+            hidden_in_steam_china=json_obj.get('hidden_in_steam_china'),
+            steam_china_location=json_obj.get('steam_china_location'),
+            application_id=json_obj.get('application_id')
+        )
+    single_user = dict(
+            steamid=user.get('steamid'),
+            num_games_owned=user.get('num_games_owned'),
+            num_reviews=user.get('num_reviews'),
+            playtime_forever=user.get('playtime_forever'),
+            playtime_last_two_weeks=user.get('playtime_last_two_weeks'),
+            playtime_at_review=user.get('playtime_at_review'),
+            last_played=user.get('last_played'),
+        )
+
+    return single_review, single_user
 
 
 def process_review_file(review_file, conn):
@@ -109,43 +153,12 @@ def process_review_file(review_file, conn):
         for line in file:
             try:
                 json_obj = json.loads(line.strip())
-                user = json_obj.get('author')
-
-                game_reviews.append(
-                    dict(
-                        recommendationid=json_obj.get('recommendationid'),
-                        author=user.get('steamid'),
-                        language=json_obj.get('language'),
-                        review=json_obj.get('review'),
-                        timestamp_created=json_obj.get('timestamp_created'),
-                        timestamp_updated=json_obj.get('timestamp_updated'),
-                        voted_up=json_obj.get('voted_up'),
-                        votes_up=json_obj.get('votes_up'),
-                        votes_funny=json_obj.get('votes_funny'),
-                        weighted_vote_score=json_obj.get('weighted_vote_score'),
-                        comment_count=json_obj.get('comment_count'),
-                        steam_purchase=json_obj.get('steam_purchase'),
-                        received_for_free=json_obj.get('received_for_free'),
-                        written_during_early_access=json_obj.get('written_during_early_access'),
-                        hidden_in_steam_china=json_obj.get('hidden_in_steam_china'),
-                        steam_china_location=json_obj.get('steam_china_location'),
-                        application_id=json_obj.get('application_id')
-                    )
-                )
-                steam_users.append(
-                    dict(
-                        steamid=user.get('steamid'),
-                        num_games_owned=user.get('num_games_owned'),
-                        num_reviews=user.get('num_reviews'),
-                        playtime_forever=user.get('playtime_forever'),
-                        playtime_last_two_weeks=user.get('playtime_last_two_weeks'),
-                        playtime_at_review=user.get('playtime_at_review'),
-                        last_played=user.get('last_played'),
-                    )
-                )
             except json.JSONDecodeError:
                 print(f"Error decoding JSON from line: {line}")
                 continue
+            single_review, single_user = parse_single_game_review(json_obj)
+            game_reviews.append(single_review)
+            steam_users.append(single_user)
     add_or_update(steam_users, steam_users_table, conn)
     add_or_update(game_reviews, game_reviews_table, conn)
 
--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+import json, os, time
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from tqdm import tqdm
+
+from icecream import ic
+
+'''
+This might be neat:
+from tqdm.contrib.discord import tqdm
+for i in tqdm(iterable, token='{token}', channel_id='{channel_id}')
+It will add the status bar to discord 
+https://tqdm.github.io/docs/contrib.discord/
+'''
+
+from database_tables import (
+    Base,
+    game_review_summary_table, games_table, game_rating_table,
+    steam_users_table, game_reviews_table
+)
+from load_json_to_database import parse_single_game_review, parse_game_json, add_or_update
 from steam_api_client import SteamAPIClient
 
+
 # Sandbox Test
-steam_api_client = SteamAPIClient()
+steam_api_client = SteamAPIClient(api_key=os.environ.get('STEAM_API_KEY'))
 app_list = steam_api_client.get_app_list()
 
 
@@ -10,7 +33,8 @@ app_list = steam_api_client.get_app_list()
         print(f'Found application {app.get("name")} at {i} {app}')
 '''
 
-import json,os,time
+upload_to_db = True
+write_json = False
 
  
 def build_set_from_json_file(file_path, key):
@@ -41,14 +65,15 @@ def build_set_from_json_file(file_path, key):
 
     return key_set
 
-already_processed_apps=build_set_from_json_file('game_reviews.json','application_id')
 
-games={}
-with open ('games.json','r') as input_games:
+# already_processed_apps = build_set_from_json_file('game_reviews.json', 'application_id')
+already_processed_apps = None
+games = {}
+with open('top100in2weeks.json', 'r') as input_games:
     games = json.load(input_games)
 
 
-apps=list(games)
+apps = list(games)
 print(len(apps))
 if already_processed_apps is not None:
     temp_apps = []
@@ -58,18 +83,54 @@ if already_processed_apps is not None:
     apps = temp_apps
 
 out_of = len(apps)
-for i,app in enumerate(apps):
-    print(f'Working on app {games[app]["name"]}:{app} ({i} of {out_of})                                                                     ',end='\r')
-    time.sleep(1)
-    response = steam_api_client.get_reviews_for_app(language='english', app_id=app, day_range=365, num_per_page=100)
-    games[app]['query_summary']=response.get('query_summary',"None")
-    with open('game_updated.json','w') as json_results:
-        json.dump(games, json_results)
-    with open("game_reviews.json","a") as json_review_results:
-        for review in response.get('reviews',[]):
-            review['application_id'] = app
-            json.dump(review, json_review_results)
-            json_review_results.write('\n')
+
+# set up the db connection
+load_dotenv()
+usr = os.environ.get('DB_USER')
+pwd = os.environ.get("DB_PWD")
+db_host = os.environ.get("DB_HOST")
+db_port = os.environ.get("DB_PORT")
+db_db = os.environ.get("DB_DATABASE")
+DATABASE_URI = f'postgresql+psycopg2://{usr}:{pwd}@{db_host}:{db_port}/{db_db}'
+engine = create_engine(DATABASE_URI)
+Base.metadata.create_all(engine)
+
+for app in tqdm(['570'] + apps):
+    time.sleep(5)
+    response = steam_api_client.get_reviews_for_app(
+        language='english',
+        app_id=app, day_range=365,
+        num_per_page=100
+    )
+    games[app]['query_summary'] = response.get('query_summary', "None")
+    #  games -> top100in2weeks.json
+    #  review -> app_review_top_100.json
+    if upload_to_db:
+        ic(response.keys())
+        ic(response['query_summary'])
+        ic(games[app])
+        sleep(10)
+        # here is looks like the only thing be updated is the query_summary
+        # with hits the games_review_summary table only
+        with engine.connect() as conn:
+            game, game_review_sum, game_rating = parse_game_json({app: games[app]})
+            add_or_update(game, games_table, conn)
+            add_or_update(game_review_sum, game_review_summary_table, conn)
+            add_or_update(game_rating, game_rating_table, conn)
+
+            for review in response.get('reviews', []):
+                review['application_id'] = app
+                single_review, single_user = parse_single_game_review(review)
+                add_or_update([single_user], steam_users_table, conn)
+                add_or_update([single_review], game_reviews_table, conn)
+    if write_json:
+        with open('game_updated.json', 'w') as json_results:
+            json.dump(games, json_results)
+        with open("game_reviews.json", "a") as json_review_results:
+            for review in response.get('reviews', []):
+                review['application_id'] = app
+                json.dump(review, json_review_results)
+                json_review_results.write('\n')
 
     
 
