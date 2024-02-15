120c6bcdda354844b0c1e5dc9be92a80555b1afb
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2024-02-15 13:27:59 -0500

    fix query summary. update to write directly to database or json

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json, os, time
 
+import click
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from tqdm import tqdm
@@ -22,6 +23,17 @@ from database_tables import (
 from load_json_to_database import parse_single_game_review, parse_game_json, add_or_update
 from steam_api_client import SteamAPIClient
 
+load_dotenv()
+
+# set up the db connection
+usr = os.environ.get('DB_USER')
+pwd = os.environ.get("DB_PWD")
+db_host = os.environ.get("DB_HOST")
+db_port = os.environ.get("DB_PORT")
+db_db = os.environ.get("DB_DATABASE")
+DATABASE_URI = f'postgresql+psycopg2://{usr}:{pwd}@{db_host}:{db_port}/{db_db}'
+engine = create_engine(DATABASE_URI)
+Base.metadata.create_all(engine)
 
 # Sandbox Test
 steam_api_client = SteamAPIClient(api_key=os.environ.get('STEAM_API_KEY'))
@@ -33,10 +45,7 @@ app_list = steam_api_client.get_app_list()
         print(f'Found application {app.get("name")} at {i} {app}')
 '''
 
-upload_to_db = True
-write_json = False
 
- 
 def build_set_from_json_file(file_path, key):
     # Check if the file exists
     if not os.path.exists(file_path):
@@ -66,74 +75,64 @@ def build_set_from_json_file(file_path, key):
     return key_set
 
 
-# already_processed_apps = build_set_from_json_file('game_reviews.json', 'application_id')
-already_processed_apps = None
-games = {}
-with open('top100in2weeks.json', 'r') as input_games:
-    games = json.load(input_games)
+@click.command()
+@click.option('--upload_to_db', is_flag=True, default=False, help='Upload data to the database')
+@click.option('--write_json', is_flag=True, default=False, help='Write data to a JSON file')
+def main(upload_to_db, write_json):
+
+    # TODO: (mjpm, 2/15/24) switch this to checking the database
+    already_processed_apps = build_set_from_json_file('game_reviews.json', 'application_id')
+    already_processed_apps = None
+    games = {}
+
+    # TODO: (mjpm, 2/15/24) this should also read from a table of unprocessed games
+    with open('games.json', 'r') as input_games:
+        games = json.load(input_games)
+
+    apps = list(games)
+    if already_processed_apps is not None:
+        temp_apps = []
+        for app in apps:
+            if app not in already_processed_apps:
+                temp_apps.append(app)
+        apps = temp_apps
+
+    for app in tqdm(apps):
+        time.sleep(0.5)
+        response = steam_api_client.get_reviews_for_app(
+            language='english',
+            app_id=app, day_range=365,
+            num_per_page=100
+        )
+        games[app]['query_summary'] = response.get('query_summary', "None")
+        #  games -> top100in2weeks.json
+        #  review -> app_review_top_100.json
+        if upload_to_db:
+            # here is looks like the only thing be updated is the query_summary
+            # with hits the games_review_summary table only
+            with engine.connect() as conn:
+                game, game_review_sum, game_rating = parse_game_json({app: games[app]})
+                add_or_update(game, games_table, conn)
+                add_or_update(game_review_sum, game_review_summary_table, conn)
+                add_or_update(game_rating, game_rating_table, conn)
+
+                for review in response.get('reviews', []):
+                    review['application_id'] = app
+                    single_review, single_user = parse_single_game_review(review)
+                    add_or_update([single_user], steam_users_table, conn)
+                    add_or_update([single_review], game_reviews_table, conn)
+        if write_json:
+            with open('game_updated.json', 'w') as json_results:
+                json.dump(games, json_results)
+            with open("game_reviews.json", "a") as json_review_results:
+                for review in response.get('reviews', []):
+                    review['application_id'] = app
+                    json.dump(review, json_review_results)
+                    json_review_results.write('\n')
 
 
-apps = list(games)
-print(len(apps))
-if already_processed_apps is not None:
-    temp_apps = []
-    for app in apps:
-        if app not in already_processed_apps:
-            temp_apps.append(app)
-    apps = temp_apps
-
-out_of = len(apps)
-
-# set up the db connection
-load_dotenv()
-usr = os.environ.get('DB_USER')
-pwd = os.environ.get("DB_PWD")
-db_host = os.environ.get("DB_HOST")
-db_port = os.environ.get("DB_PORT")
-db_db = os.environ.get("DB_DATABASE")
-DATABASE_URI = f'postgresql+psycopg2://{usr}:{pwd}@{db_host}:{db_port}/{db_db}'
-engine = create_engine(DATABASE_URI)
-Base.metadata.create_all(engine)
-
-for app in tqdm(['570'] + apps):
-    time.sleep(5)
-    response = steam_api_client.get_reviews_for_app(
-        language='english',
-        app_id=app, day_range=365,
-        num_per_page=100
-    )
-    games[app]['query_summary'] = response.get('query_summary', "None")
-    #  games -> top100in2weeks.json
-    #  review -> app_review_top_100.json
-    if upload_to_db:
-        ic(response.keys())
-        ic(response['query_summary'])
-        ic(games[app])
-        sleep(10)
-        # here is looks like the only thing be updated is the query_summary
-        # with hits the games_review_summary table only
-        with engine.connect() as conn:
-            game, game_review_sum, game_rating = parse_game_json({app: games[app]})
-            add_or_update(game, games_table, conn)
-            add_or_update(game_review_sum, game_review_summary_table, conn)
-            add_or_update(game_rating, game_rating_table, conn)
-
-            for review in response.get('reviews', []):
-                review['application_id'] = app
-                single_review, single_user = parse_single_game_review(review)
-                add_or_update([single_user], steam_users_table, conn)
-                add_or_update([single_review], game_reviews_table, conn)
-    if write_json:
-        with open('game_updated.json', 'w') as json_results:
-            json.dump(games, json_results)
-        with open("game_reviews.json", "a") as json_review_results:
-            for review in response.get('reviews', []):
-                review['application_id'] = app
-                json.dump(review, json_review_results)
-                json_review_results.write('\n')
-
-    
-
+if __name__ == '__main__':
+    main()
 
 
 
