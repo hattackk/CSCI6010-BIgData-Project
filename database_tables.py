1539fc52435b115a91859fb03a144f2733a1537c
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2024-02-17 07:47:58 -0500

    update database update ddl update user

--- database_tables.py
+++ database_tables.py
@@ -2,10 +2,15 @@ from sqlalchemy import Column, Integer, Float, String, MetaData, Table
 from sqlalchemy import BigInteger, ForeignKey, Boolean
 from sqlalchemy.orm import declarative_base
 
-
-Base = declarative_base()
 metadata = MetaData()
 
+# putting this at the top since it wasn't in the original file
+# this table will be used for tracking the status of the game review downloads
+game_review_download_status_table = Table(
+    'game_review_download_status', metadata,
+    Column('game_id', Integer, primary_key=True),
+    Column('status', String)
+)
 
 games_table = Table(
     'games', metadata,
@@ -32,7 +37,7 @@ game_rating_table = Table(
     Column('average_2weeks', Integer),
     Column('median_forever', Integer),
     Column('median_2weeks', Integer)
-                   )
+)
 
 game_review_summary_table = Table(
     'game_review_summary', metadata,
@@ -48,30 +53,23 @@ game_review_summary_table = Table(
 
 game_reviews_table = Table(
     'game_reviews', metadata,
-     Column('recommendationid', BigInteger, primary_key=True),
-     Column('author', BigInteger),
-     Column('language', String),
-     Column('review', String),
-     Column('timestamp_created', Integer),
-     Column('timestamp_updated', Integer),
-     Column('voted_up', Boolean),
-     Column('votes_up', Integer),
-     Column('votes_funny', Integer),
-     Column('weighted_vote_score', Float),
-     Column('comment_count', Integer),
-     Column('steam_purchase', Boolean),
-     Column('received_for_free', Boolean),
-     Column('written_during_early_access', Boolean),
-     Column('hidden_in_steam_china', Boolean),
-     Column('steam_china_location', String),
-     Column('application_id', Integer, ForeignKey('games.game_id'))
-)
-
-steam_users_table = Table(
-    'steam_users', metadata,
-    Column('steamid', BigInteger, primary_key=True),
-    Column('num_games_owned', Integer),
-    Column('num_reviews', Integer),
+    Column('recommendationid', BigInteger, primary_key=True),
+    Column('steamid', BigInteger),
+    Column('language', String),
+    Column('review', String),
+    Column('timestamp_created', Integer),
+    Column('timestamp_updated', Integer),
+    Column('voted_up', Boolean),
+    Column('votes_up', BigInteger),
+    Column('votes_funny', BigInteger),
+    Column('weighted_vote_score', Float),
+    Column('comment_count', Integer),
+    Column('steam_purchase', Boolean),
+    Column('received_for_free', Boolean),
+    Column('written_during_early_access', Boolean),
+    Column('hidden_in_steam_china', Boolean),
+    Column('steam_china_location', String),
+    Column('application_id', Integer, ForeignKey('games.game_id')),
     Column('playtime_forever', Integer),
     Column('playtime_last_two_weeks', Integer),
     Column('playtime_at_review', Integer),
@@ -79,9 +77,9 @@ steam_users_table = Table(
 )
 
 
-
-game_review_download_status_table = Table(
-    'game_review_download_status', metadata,
-    Column('game_id', Integer, primary_key=True),
-    Column('status', String),
+steam_users_table = Table(
+    'steam_users', metadata,
+    Column('steamid', BigInteger, primary_key=True),
+    Column('num_games_owned', Integer),
+    Column('num_reviews', Integer),
 )
