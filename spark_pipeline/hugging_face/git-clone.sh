408002830806c22bcc437115d77c62bb5f617ed3
cmurp <rajonwitness@gmail.com>
2024-03-12 11:03:04 -0400

    added dockerfile for running spark pipeline

--- spark_pipeline/hugging_face/git-clone.sh
+++ spark_pipeline/hugging_face/git-clone.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+cd "/"
 
 # Get the directory where the script resides
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Script directory: $script_dir"
 
 # File containing repository URLs (one per line)
-url_file="model_repo_urls.txt"  
+url_file="./model_repo_urls.txt"  
 
 # Check if the file exists
 if [ ! -f "$url_file" ]; then
@@ -18,7 +19,6 @@ echo "Reading files..."
 # Read each line from the file and clone the repository
 while IFS= read -r url || [[ -n "$url" ]]; do
     echo "Cloning repository from: $url"
-    cd "$script_dir"
     git clone "$url"
     # Check if the clone was successful
     if [ $? -eq 0 ]; then
