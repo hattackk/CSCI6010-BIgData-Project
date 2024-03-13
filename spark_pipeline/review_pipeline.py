77a2d42038e67c99a36b55f219ae5de9c812190d
cmurp <rajonwitness@gmail.com>
2024-03-12 22:58:56 -0400

    cleanup

--- spark_pipeline/review_pipeline.py
+++ spark_pipeline/review_pipeline.py
@@ -1,41 +1,12 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, LongType, StringType, IntegerType, BooleanType, FloatType
+from pyspark.sql.types import IntegerType
 from spam_filter_udf import spam_filter
-import generate_fake_review
 from pyspark.sql.functions import udf
 from pyspark.sql.types import IntegerType
-from pyspark import SparkFiles
 import psycopg2
 import pandas as pd
 import os
 
-# Define schema for game_reviews table
-schema = StructType([
-    StructField("recommendationid", LongType(), nullable=False),
-    StructField("steamid", LongType(), nullable=False),
-    StructField("language", StringType(), nullable=False),
-    StructField("review", StringType(), nullable=False),
-    StructField("timestamp_created", IntegerType(), nullable=False),
-    StructField("timestamp_updated", IntegerType(), nullable=False),
-    StructField("voted_up", BooleanType(), nullable=False),
-    StructField("votes_up", LongType(), nullable=False),
-    StructField("votes_funny", LongType(), nullable=False),
-    StructField("weighted_vote_score", FloatType(), nullable=False),
-    StructField("comment_count", IntegerType(), nullable=False),
-    StructField("steam_purchase", BooleanType(), nullable=False),
-    StructField("received_for_free", BooleanType(), nullable=False),
-    StructField("written_during_early_access", BooleanType(), nullable=False),
-    StructField("hidden_in_steam_china", BooleanType(), nullable=False),
-    StructField("steam_china_location", StringType(), nullable=False),
-    StructField("application_id", IntegerType(), nullable=False),
-    StructField("playtime_forever", IntegerType(), nullable=False),
-    StructField("playtime_last_two_weeks", IntegerType(), nullable=False),
-    StructField("playtime_at_review", IntegerType(), nullable=False),
-    StructField("last_played", IntegerType(), nullable=False)
-])
-
-# Generate test data for 5 reviews
-test_data = [generate_fake_review.generate_review() for _ in range(5)]
 
 # Initialize SparkSession
 spark = SparkSession.builder \
@@ -77,13 +48,9 @@ conn = psycopg2.connect(
     port=db_port
 )
 
-# Create a cursor object
+
 cur = conn.cursor()
-
-# Execute a query
 cur.execute("SELECT * FROM game_reviews")
-
-# Fetch all rows from the database
 rows = cur.fetchall()
 
 # Convert to pandas DataFrame
@@ -98,6 +65,6 @@ spam_udf = udf(spam_filter,IntegerType())
 processed_df = sdf.withColumn("is_spam", spam_udf(sdf["review"]))
 
 processed_df.select("review", "is_spam") \
-    .show(truncate=False)
+    .show(truncate=80, n=100)
 
 spark.stop()
