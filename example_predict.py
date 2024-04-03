21ea9eda352f74ec4804ee9fc43ed88cc5238175
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2024-04-03 10:36:44 -0400

    finishh the model building

---
+++ example_predict.py
@@ -0,0 +1,9 @@
+from model import RecommenderModel
+from numpy.random import randint
+from tabulate import tabulate
+model = RecommenderModel.load('./recommender_model.pkl.xz')
+users = list(model.user_index_mapping.keys())
+user_id = users[randint(0, len(users))]
+print(f'user id: {users[randint(0, len(users))]}')
+recommendation = model.predict(user_id)
+print(tabulate(recommendation, showindex=False, headers='keys', tablefmt='psql'))
